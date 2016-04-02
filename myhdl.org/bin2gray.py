f0d3a3529fbf0d585e39f81a4f03f894401d9d9c
Andreas Mueller <andreas.mueller2207@web.de>
2016-04-02 16:14:09 +0200

    Add bin2gray as example for MyHDL bit indexing

---
+++ myhdl.org/bin2gray.py
@@ -0,0 +1,42 @@
+#!/bin/python
+#
+# File: bin2gray.py
+# Date: 2016-04-01
+# Author: Andreas Mueller
+#
+# Description: Example for MyHDL bit indexing from myhdl.org.
+#
+
+from myhdl import Signal, delay, Simulation, always_comb, instance, intbv, bin
+
+def bin2gray(bin_i, gray_o, WIDTH):
+    """Binary to gray encoder."""
+
+    @always_comb
+    def logic():
+        for i in range(WIDTH):
+            gray_o.next[i] = bin_i[i+1] ^ bin_i[i]
+
+    return logic
+
+
+def bin2gray_tb(WIDTH):
+    """Testbench for bin2gray."""
+
+    bin_i = Signal(intbv(0))
+    gray_o = Signal(intbv(0))
+
+    bin2gray_dut = bin2gray(bin_i=bin_i, gray_o=gray_o, WIDTH=WIDTH)
+
+    @instance
+    def stimulus():
+        for i in range(2**WIDTH):
+            bin_i.next = intbv(i)
+            yield delay(10)
+            print("bin_i {0} | gray_o {1}".format(bin(bin_i, WIDTH), bin(gray_o, WIDTH)))
+
+    return bin2gray_dut, stimulus
+
+
+sim = Simulation(bin2gray_tb(WIDTH=3))
+sim.run()
